3bbbc288799d5516b0a68a80b502a470074497f2
BrunoEduardo1 <brunoeduardocoe@gmail.com>
2018-10-21 10:03:40 -0300

    Rename pages default to pagess

--- config/routes.php
+++ config/routes.php
@@ -52,7 +52,7 @@ Router::scope('/', function (RouteBuilder $routes) {
      * its action called 'display', and we pass a param to select the view file
      * to use (in this case, src/Template/Pages/home.ctp)...
      */
-    $routes->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
+    $routes->connect('/', ['controller' => 'Pagess', 'action' => 'display', 'home']);
     //página de informações
     $routes->connect('/informations', ['controller' => 'Informations', 'action' => 'add']);
     /**
